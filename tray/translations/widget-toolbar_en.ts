9b02229c17f1a6ea13797bbf5c1ec5b70da07994
燕戏竹林 <yxzl666xx@outlook.com>
2026-08-16 05:50:50 +0800

    chore(i18n): refresh tray translation locations

--- tray/translations/widget-toolbar_en.ts
+++ tray/translations/widget-toolbar_en.ts
@@ -12,27 +12,27 @@
 <context>
     <name>WidgetToolbarTrayPlugin</name>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="43"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="44"/>
         <source>Widget Toolbar</source>
         <translation>Widget Toolbar</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="95"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="107"/>
         <source>Settings</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="96"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="108"/>
         <source>About</source>
         <translation>About</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="97"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="109"/>
         <source>Add widget</source>
         <translation>Add widget</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="98"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="110"/>
         <source>Arrange</source>
         <translation>Arrange</translation>
     </message>
--- tray/translations/widget-toolbar_zh_CN.ts
+++ tray/translations/widget-toolbar_zh_CN.ts
@@ -19,27 +19,27 @@
 <context>
     <name>WidgetToolbarTrayPlugin</name>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="43"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="44"/>
         <source>Widget Toolbar</source>
         <translation>小组件工具栏</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="95"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="107"/>
         <source>Settings</source>
         <translation>设置</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="96"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="108"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="97"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="109"/>
         <source>Add widget</source>
         <translation>添加组件</translation>
     </message>
     <message>
-        <location filename="../widgettoolbartrayplugin.cpp" line="98"/>
+        <location filename="../widgettoolbartrayplugin.cpp" line="110"/>
         <source>Arrange</source>
         <translation>整理</translation>
     </message>
