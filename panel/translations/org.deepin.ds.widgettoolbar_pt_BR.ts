93204abb1dc76bc2d8209f48c3c2f3224909d0e5
燕戏竹林 <yxzl666xx@outlook.com>
2026-09-10 01:34:39 +0800

    chore(translations): update translations for the app launcher widget
    Refresh all .ts files via lupdate: new strings for the app picker,
    launcherList settings rows and the 4x1 size option, plus refreshed
    line locations.

--- panel/translations/org.deepin.ds.widgettoolbar_pt_BR.ts
+++ panel/translations/org.deepin.ds.widgettoolbar_pt_BR.ts
@@ -4,26 +4,32 @@
 <context>
     <name>AboutPopup</name>
     <message>
+        <location filename="../package/AboutPopup.qml" line="40"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AboutPopup.qml" line="55"/>
         <source>Developer team</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AboutPopup.qml" line="63"/>
         <source>Version</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AboutPopup.qml" line="71"/>
         <source>License</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AboutPopup.qml" line="81"/>
         <source>Repository</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AboutPopup.qml" line="92"/>
         <source>Website</source>
         <translation type="unfinished"></translation>
     </message>
@@ -31,61 +37,121 @@
 <context>
     <name>AddWidgetPopup</name>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="72"/>
         <source>Add widgets</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="80"/>
         <source>Available widgets</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="165"/>
         <source>Added</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="165"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="186"/>
         <source>Added widgets</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="244"/>
         <source>Uninstall</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="253"/>
         <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="280"/>
         <source>Install from .dwpkg file…</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="288"/>
         <source>Select a widget package</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="289"/>
         <source>Widget packages</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="292"/>
         <source>Widget installed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/AddWidgetPopup.qml" line="294"/>
         <source>Install failed: invalid or unsafe package</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AppPickerBody</name>
+    <message>
+        <location filename="../package/widgets/components/AppPickerBody.qml" line="107"/>
+        <source>Choose app</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/AppPickerBody.qml" line="117"/>
+        <source>Search apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/AppPickerBody.qml" line="238"/>
+        <source>No matching apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/AppPickerBody.qml" line="253"/>
+        <source>Selected: %1/%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/AppPickerBody.qml" line="264"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/AppPickerBody.qml" line="269"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColorPickerDialog</name>
+    <message>
+        <location filename="../package/widgets/components/ColorPickerDialog.qml" line="237"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/ColorPickerDialog.qml" line="245"/>
+        <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>PinButton</name>
     <message>
+        <location filename="../package/PinButton.qml" line="40"/>
         <source>Unpin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/PinButton.qml" line="40"/>
         <source>Pin to top</source>
         <translation type="unfinished"></translation>
     </message>
@@ -93,197 +159,275 @@
 <context>
     <name>SettingsDialog</name>
     <message>
+        <location filename="../package/SettingsDialog.qml" line="92"/>
         <source>Card transparent mode</source>
         <translation type="unfinished"></translation>
     </message>
-
     <message>
+        <location filename="../package/SettingsDialog.qml" line="39"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/SettingsDialog.qml" line="50"/>
         <source>Show panel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/SettingsDialog.qml" line="72"/>
         <source>Pin to top</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsRow</name>
+    <message>
+        <location filename="../package/widgets/components/SettingsRow.qml" line="497"/>
+        <source>Hidden: %1 more (restored when the card is enlarged)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/SettingsRow.qml" line="706"/>
+        <source>Restore defaults</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/widgets/components/SettingsRow.qml" line="714"/>
+        <location filename="../package/widgets/components/SettingsRow.qml" line="804"/>
+        <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>WidgetSettingsPopup</name>
     <message>
+        <location filename="../package/WidgetSettingsPopup.qml" line="407"/>
         <source>Widget settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/WidgetSettingsPopup.qml" line="455"/>
         <source>This widget has no configurable options</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="237"/>
         <source>System Monitor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="76"/>
         <source>CPU</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="77"/>
         <source>MEM</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="78"/>
         <source>DISK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/todo/main.qml" line="341"/>
+        <location filename="../package/widgets/todo/main.qml" line="350"/>
         <source>Sticky Note</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/todo/main.qml" line="434"/>
         <source>Write something…</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/worldtime/main.qml" line="314"/>
         <source>World Time</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="36"/>
+        <location filename="../package/widgets/calendar/main.qml" line="37"/>
         <source>Sun</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="35"/>
+        <location filename="../package/widgets/calendar/main.qml" line="37"/>
         <source>Mon</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="35"/>
+        <location filename="../package/widgets/calendar/main.qml" line="37"/>
         <source>Tue</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="35"/>
+        <location filename="../package/widgets/calendar/main.qml" line="37"/>
         <source>Wed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="35"/>
+        <location filename="../package/widgets/calendar/main.qml" line="38"/>
         <source>Thu</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="36"/>
+        <location filename="../package/widgets/calendar/main.qml" line="38"/>
         <source>Fri</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="36"/>
+        <location filename="../package/widgets/calendar/main.qml" line="38"/>
         <source>Sat</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/calendar/main.qml" line="88"/>
+        <location filename="../package/widgets/calendar/main.qml" line="98"/>
         <source>yyyy MMMM</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="575"/>
         <source>Widget Toolbar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="725"/>
         <source>No widgets yet</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="757"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="743"/>
+        <location filename="../package/main.qml" line="777"/>
         <source>Arrange</source>
         <translation>Organizar</translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="773"/>
         <source>Add widget</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="782"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="786"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/lyrics/main.qml" line="38"/>
         <source>Start Ter-Music and play a song to show lyrics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/lyrics/main.qml" line="40"/>
         <source>Ter-Music is not playing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/lyrics/main.qml" line="42"/>
         <source>This track has no lyrics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/lyrics/main.qml" line="76"/>
         <source>Ter-Music Lyrics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="859"/>
         <source>Small</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="860"/>
         <source>Medium</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="861"/>
+        <source>Long</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../package/main.qml" line="862"/>
         <source>Wide</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="863"/>
         <source>Large</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="888"/>
         <source>Settings…</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/main.qml" line="894"/>
         <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="79"/>
         <source>GPU</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="116"/>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="118"/>
         <source>N/A</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="80"/>
         <source>NPU</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/systemmonitor/main.qml" line="279"/>
         <source>No metrics enabled</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/worldtime/main.qml" line="297"/>
         <source>No dials yet. Add timezones in widget settings.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/player/main.qml" line="220"/>
+        <location filename="../package/widgets/player/main.qml" line="292"/>
         <source>Unknown title</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/player/main.qml" line="232"/>
+        <location filename="../package/widgets/player/main.qml" line="306"/>
         <source>Unknown artist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../package/widgets/player/main.qml" line="148"/>
         <source>No song playing</source>
         <translation type="unfinished"></translation>
     </message>
